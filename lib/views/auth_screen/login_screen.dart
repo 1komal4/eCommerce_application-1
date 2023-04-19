c112e8a0bf3b0d2ee3218d89729c43b9fa468761
lyricMirror <nirajsahu0412@gmail.com>
2023-04-19 18:56:20 +0530

    commit

--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_application/consts/consts.dart';
 import 'package:ecommerce_application/widget_common/applogo.dart';
 import 'package:ecommerce_application/widget_common/bg_widget.dart';
+import 'package:ecommerce_application/widget_common/custom_textfield.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -17,6 +18,19 @@ class LoginScreen extends StatelessWidget {
             10.heightBox,
             "Login in to $appName".text.white.size(18).make(),
             10.heightBox,
+            Column(
+              children: [
+                customTextFeild(email, emailHint),
+                customTextFeild(password, passwordhint),
+                TextButton(onPressed: () {}, child: forgetPass.text.make())
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 70)
+                .make(),
           ],
         ),
       ),
--- lib/widget_common/custom_textfield.dart
+++ lib/widget_common/custom_textfield.dart
@@ -1,7 +1,31 @@
 import 'package:ecommerce_application/consts/consts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-Widget customTextFeild() {
+Widget customTextFeild(String? title, String? hint) {
   return Column(
-    children: [],
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      title!.text.color(Colors.red).size(16).make(),
+      5.heightBox,
+      TextFormField(
+        //controller: controller,
+        decoration: InputDecoration(
+          hintStyle: const TextStyle(
+            color: Color.fromARGB(255, 141, 140, 140),
+          ),
+          hintText: hint,
+          isDense: true,
+          fillColor: Color.fromARGB(255, 238, 238, 238),
+          filled: true,
+          border: InputBorder.none,
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+      5.heightBox,
+    ],
   );
 }
