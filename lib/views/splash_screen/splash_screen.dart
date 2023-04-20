dbd870cecc9b6b08748562aca7e4501c6b57334f
1komal4 <111501828+1komal4@users.noreply.github.com>
2023-04-20 11:28:48 +0530

    commit

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -31,7 +31,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Colors.black,
       body: Center(
           child: Column(
         children: [
